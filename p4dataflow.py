3d00eb91ff450919223482539593fd6893fe92ce
Diogo Campos <diogocampos@users.noreply.github.com>
2019-06-09 20:17:46 -0300

    Continue implementation of 'parsers'

--- p4dataflow.py
+++ p4dataflow.py
@@ -36,39 +36,44 @@ def do_headers(p4, flow):
 
 
 def do_parsers(p4, flow):
-    parser = p4['parsers'][0]
+    parser = p4['parsers'][0]  # pode haver mais de um ???
     state = find(
         parser['parse_states'],
         lambda state: state['name'] == parser['init_state'],
     )
 
     while state is not None:
-        print('STATE:', state['name'])
-
         op, *_ = state['parser_ops']
-        if op['op'] != 'extract':
+
+        if op['op'] == 'extract':
+            op_extract(op, flow)
+        elif op['op'] == 'set':
+            op_set(op, flow)
+        elif op['op'] == 'verify':
+            op_verify(op, flow)
+        else:
             raise NotImplementedError(f"op: {op['op']}")
 
-        param, = op['parameters']
-        if param['type'] != 'regular':
-            raise NotImplementedError(f"parameter type: {param['type']}")
+        if len(state['transition_key']) != 1:
+            raise NotImplementedError(f'Multiple/missing transition_key items')
 
-        sequence = flow[param['value']]
-        for seq in sequence.values(): seq.append('D')
+        tk, = state['transition_key']
+        transition = None
 
-        for tk in state['transition_key']:
-            if tk['type'] != 'field':
-                raise NotImplementedError(f"transition_key type: {tk['type']}")
+        if tk['type'] == 'field':
+            append(flow, tk['value'], 'U')
 
-            header, field = tk['value']
-            flow[header][field].append('U')
+            #vtk = valor de tk['value']  # ???
 
-        for transition in state['transitions']:
-            if 'type' not in transition:
-                break
-            elif transition['type'] == 'hexstr':
-                # if transition['value'] == vtf: break  # ???
-                break
+            for transition in state['transitions']:
+                if 'type' not in transition or transition['type'] == 'default':
+                    break
+                elif transition['type'] == 'hexstr':
+                    # if transition['value'] == vtk: break  # ???
+                    pass
+
+        else:
+            raise NotImplementedError(f"transition_key type: {tk['type']}")
 
         next_state = transition['next_state']
         if next_state is None: break
@@ -79,8 +84,55 @@ def do_parsers(p4, flow):
         )
 
 
-def find(list, predicate):
-    for item in list:
+def op_extract(op, flow):
+    param, = op['parameters']
+
+    if param['type'] == 'regular':
+        sequences = flow[param['value']]
+        for seq in sequences.values(): seq.append('D')
+
+    else:
+        raise NotImplementedError(f"parameter type: {param['type']}")
+
+
+def op_set(op, flow):
+    left, right = op['parameters']
+    append(flow, left['value'], 'D')
+    operation(right, flow)
+
+
+def op_verify(op, flow):
+    assertion, errno = op['parameters']
+    operation(assertion, flow)
+    operation(errno, flow)
+
+
+def operation(node, flow):
+    if node is None: return
+
+    if node['type'] == 'field':
+        append(flow, node['value'], 'U')
+
+    elif node['type'] == 'expression':
+        expression(node['value'], flow)
+
+
+def expression(expr, flow):
+    left = expr['left']
+    right = expr['right']
+    cond = expr['cond'] if 'cond' in expr else None
+
+    for part in (left, right, cond):
+        operation(part, flow)
+
+
+def append(flow, field, du):
+    header_name, field_name = field
+    flow[header_name][field_name].append(du)
+
+
+def find(iterable, predicate):
+    for item in iterable:
         if predicate(item): return item
 
 
