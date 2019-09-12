513a5fe2b969ae12a9cc6bbb868c27e6b061f762
Diogo Campos <diogocampos@users.noreply.github.com>
2019-09-12 10:19:22 -0300

    Traverse all actions in 'pipelines'

--- p4df/pipelines.py
+++ p4df/pipelines.py
@@ -9,7 +9,7 @@ def do_pipelines(p4, flow):
 def _pipeline(pipeline_name, p4, flow):
     pipeline = find(p4['pipelines'], name=pipeline_name)
     bfs = BreadthFirstSearch(pipeline['init_table'])
-    flow.set_prefix(pipeline_name)
+    flow.set_prefix(pipeline['name'])
     flow.add_transitions([pipeline['init_table']])
 
     for table_name in bfs:
@@ -24,12 +24,20 @@ def _pipeline(pipeline_name, p4, flow):
                 raise NotImplementedError('pipelines - tarefa 2')
 
             flow.use(key['target'])
-            # match_type true/false ??
 
-            for action_name in table['actions']:
-                flow.push_node(f"{table_name}/{action_name}")
+            # match
+            actions = [
+                (True, find(p4['actions'], name=action_name))
+                for action_name in table['actions']]
 
-                action = find(p4['actions'], name=action_name)
+            # no match
+            actions.append(
+                (False, find(p4['actions'], id=table['default_entry']['action_id']))
+                )
+
+            for matched, action in actions:
+                match = 'Match' if matched else 'NoMatch'
+                flow.push_node(f"{table['name']}/{match}/{action['name']}")
 
                 flow.declare_header(action['name'])
                 for item in action['runtime_data']:
@@ -54,10 +62,10 @@ def _pipeline(pipeline_name, p4, flow):
                     #else:
                     #    raise NotImplementedError('pipelines - tarefa 3/4')
 
-                next_tables = [table['next_tables'][action_name]]
+                next_tables = [table['next_tables'][action['name']]]
                 flow.add_transitions(next_tables)
                 bfs.enqueue(next_tables)
-                flow.set_current_node(table_name)
+                flow.set_current_node(table['name'])
 
         else:
             conditional = find(pipeline['conditionals'], name=table_name)
